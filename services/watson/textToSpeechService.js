badd22bfaff078ec807a71d953c44c13d9b0980b
Fernando Almeida <fernando.c.almeida@gmail.com>
2018-03-19 21:03:34 -0300

    daily-update

--- services/watson/textToSpeechService.js
+++ services/watson/textToSpeechService.js
@@ -7,24 +7,24 @@ var Speaker = require('speaker');
 const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 
 const textToSpeech = new TextToSpeechV1({
-  username: '<username>',
-  password: '<password>',
+  username: '89163d74-3d65-41e3-b97e-879bfc240c50',
+  password: 'Kq4aGlRIcErq',
   version: v1
 });
 
 function process(parameters)
 {
     var params = {
-        text = parameters[0],
-        accept: 'audio/ogg; codec=opus'
+        "text": parameters[0],
+        "accept": 'audio/ogg; codec=opus'
     }
 
     textToSpeech.synthesize(params).
         pipe(new ogg.Decoder()).
         on('stream', function (opusStream) {
-            opusStream.pipe(new opus.Decoder()).
+            opusStream.pipe(new ogg.Decoder()).
             pipe(new Speaker());
-});
+            });
 }
 
 module.exports = { process };
